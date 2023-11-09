dd778d186061a31d93741d383dd8a7c1996f2889
Romit-Kumar18 <113458536+Romit-Kumar18@users.noreply.github.com>
2023-11-09 16:38:44 +0530

    final working (with comments)

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def get_folder_sizes(path):
     for thread in threads:
         thread.join()
     print(result_dict)
-    return result_dict
+    sorted_result_dict = dict(sorted(result_dict.items(), key=lambda item: os.path.split(item[0].replace('\\', '/'))[0].split('/')[0]))
+    return sorted_result_dict
 
 # def get_file_sizes(directory):
 #     file_sizes = defaultdict(list)
@@ -74,12 +75,40 @@ def assign_colors_to_subdirectories(file_sizes):
     color_index = 0
 
     for relative_path in file_sizes.keys():
-        subdirectory, _ = os.path.split(relative_path.replace('\\', '/'))  # Replace backslashes with forward slashes
-        if subdirectory not in subdirectory_colors:
-            subdirectory_colors[subdirectory] = sns.color_palette("Set1", n_colors=len(subdirectory_colors) + 1)[color_index]
+        subdirectories = os.path.split(relative_path.replace('\\', '/'))[0].split('/')  # Get all subdirectories
+        top_level_subdirectory = subdirectories[0]  # Get the top-level subdirectory
+
+        if top_level_subdirectory not in subdirectory_colors:
+            subdirectory_colors[top_level_subdirectory] = sns.color_palette("Set1", n_colors=len(subdirectory_colors) + 1)[color_index]
             color_index += 1
 
     return subdirectory_colors
+
+# def assign_colors_to_subdirectories(file_sizes):
+#     subdirectory_colors = {}
+#     color_index = 0
+
+#     for relative_path in file_sizes.keys():
+#         subdirectory, _ = os.path.split(relative_path.replace('\\', '/'))  # Replace backslashes with forward slashes
+#         top_level_subdirectory = subdirectory.split('/')[0]  # Get the top-level subdirectory
+
+#         if top_level_subdirectory not in subdirectory_colors:
+#             subdirectory_colors[top_level_subdirectory] = sns.color_palette("Set1", n_colors=len(subdirectory_colors) + 1)[color_index]
+#             color_index += 1
+
+#     return subdirectory_colors
+
+# def assign_colors_to_subdirectories(file_sizes):
+#     subdirectory_colors = {}
+#     color_index = 0
+
+#     for relative_path in file_sizes.keys():
+#         subdirectory, _ = os.path.split(relative_path.replace('\\', '/'))  # Replace backslashes with forward slashes
+#         if subdirectory not in subdirectory_colors:
+#             subdirectory_colors[subdirectory] = sns.color_palette("Set1", n_colors=len(subdirectory_colors) + 1)[color_index]
+#             color_index += 1
+
+#     return subdirectory_colors
 
 def create_legend(ax, subdirectory_colors, file_sizes):
     legend_patches = []
@@ -87,13 +116,28 @@ def create_legend(ax, subdirectory_colors, file_sizes):
     # Add files to legend, including their relative paths
     for relative_path, files in file_sizes.items():
         for file, size in files:
-            subdirectory, _ = os.path.split(relative_path.replace('\\', '/'))  # Replace backslashes with forward slashes
-            legend_patches.append(Patch(color=subdirectory_colors[subdirectory], label=f"{relative_path} - {file} ({convert_size(size)})"))
+            subdirectories = os.path.split(relative_path.replace('\\', '/'))[0].split('/')  # Get all subdirectories
+            top_level_subdirectory = subdirectories[0]  # Get the top-level subdirectory
+            legend_patches.append(Patch(color=subdirectory_colors[top_level_subdirectory], label=f"{relative_path} - {file} ({convert_size(size)})"))
             count+=1
         if count>40:
             print("Legend truncated due to size limit.")
             break
     ax.legend(handles=legend_patches, loc='upper left', bbox_to_anchor=(1, 1), title="Legend", fontsize='small')
+
+# def create_legend(ax, subdirectory_colors, file_sizes):
+#     legend_patches = []
+#     count=0
+#     # Add files to legend, including their relative paths
+#     for relative_path, files in file_sizes.items():
+#         for file, size in files:
+#             subdirectory, _ = os.path.split(relative_path.replace('\\', '/'))  # Replace backslashes with forward slashes
+#             legend_patches.append(Patch(color=subdirectory_colors[subdirectory], label=f"{relative_path} - {file} ({convert_size(size)})"))
+#             count+=1
+#         if count>40:
+#             print("Legend truncated due to size limit.")
+#             break
+#     ax.legend(handles=legend_patches, loc='upper left', bbox_to_anchor=(1, 1), title="Legend", fontsize='small')
 
 def plot_treemap(file_sizes):
     labels = []
@@ -105,8 +149,9 @@ def plot_treemap(file_sizes):
     for idx, (relative_path, files) in enumerate(file_sizes.items()):
         labels.append(relative_path)
         sizes.append(sum(size for _, size in files))
-        subdirectory, _ = os.path.split(relative_path.replace('\\', '/'))  # Replace backslashes with forward slashes
-        colors.append(subdirectory_colors[subdirectory])
+        subdirectories = os.path.split(relative_path.replace('\\', '/'))[0].split('/')  # Get all subdirectories
+        top_level_subdirectory = subdirectories[0]  # Get the top-level subdirectory
+        colors.append(subdirectory_colors[top_level_subdirectory])
 
     total_size = sum(sizes)
 
@@ -132,6 +177,44 @@ def plot_treemap(file_sizes):
         plt.show()
     else:
         print("Total size is zero. Cannot create treemap.")
+
+# def plot_treemap(file_sizes):
+#     labels = []
+#     sizes = []
+#     colors = []
+
+#     subdirectory_colors = assign_colors_to_subdirectories(file_sizes)
+
+#     for idx, (relative_path, files) in enumerate(file_sizes.items()):
+#         labels.append(relative_path)
+#         sizes.append(sum(size for _, size in files))
+#         subdirectory, _ = os.path.split(relative_path.replace('\\', '/'))  # Replace backslashes with forward slashes
+#         colors.append(subdirectory_colors[subdirectory])
+
+#     total_size = sum(sizes)
+
+#     if total_size > 0:
+#         norm_sizes = [(size / total_size) * 100 for size in sizes]
+
+#         # Create treemap
+#         fig, ax = plt.subplots(figsize=(12, 10))
+#         squarify.plot(sizes=norm_sizes, label=None, color=colors, alpha=0.7, ax=ax)
+
+#         # Add border to each rectangle
+#         for rect, color in zip(ax.patches, colors):
+#             rect.set_linewidth(1)
+#             rect.set_edgecolor('black')
+
+#         # Create a legend for files including their relative paths
+#         create_legend(ax, subdirectory_colors, file_sizes)
+
+#         # Adjust layout
+#         plt.axis('off')
+#         plt.tight_layout()
+
+#         plt.show()
+#     else:
+#         print("Total size is zero. Cannot create treemap.")
 
 def main():
     directory = input("Enter the path of the directory: ")
